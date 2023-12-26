b78d30f39d7e22f7c6f5f40d3d9c6ab1ff708576
Jay <jonnygehlot01@gmail.com>
2023-12-26 13:10:45 +0530

    update

--- js/main.js
+++ js/main.js
@@ -290,4 +290,6 @@ HtL.addEventListener('click', () => {
         })
 
         .catch(error => console.error('Error fetching data:', error));
-})
+});
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
